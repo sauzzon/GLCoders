733d164c941adeb193f48bae0a8469a89c358306
Saujan Tiwari <saujan.tiwari@gmail.com>
2021-08-23 18:49:19 +0545

    minor commit
    minor commit

--- maincpp.h
+++ maincpp.h
@@ -127,7 +127,7 @@ const unsigned int SCR_WIDTH = 3840;
 const unsigned int SCR_HEIGHT = 2400;
 
 // camera
-Camera camera(Transf::vec3(0.0f, 0.0f, 15.0f));
+Camera camera(Transf::vec3(0.0f, 0.0f, 30.0f));
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
